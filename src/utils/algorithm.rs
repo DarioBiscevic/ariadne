bebc7c6f47c15cc96e9acb8548a755e4e2c589df
Dario Biscevic <dario.biscevic03@gmail.com>
2022-11-30 20:51:39 +0100

    Refactored

--- src/utils/algorithm.rs
+++ src/utils/algorithm.rs
@@ -38,46 +38,46 @@ fn dijkstra(root: &Rc<RefCell<Node>>, n_nodes: usize) -> Result<Path>{
 
     //Loop while there are nodes (and subsequent paths) to expand
     while !path_edges.is_empty(){
-
-        //Remove all the nodes that were already checked - they won't be used again
-        path_edges.retain(|n| !n.borrow().seen);
-
         //Sort the vector and get the node with the smallest tentative distance
         path_edges.sort();
-        let current = path_edges[0].clone();
+        let current_rc = path_edges[0].clone();
 
-        let mut current = current.borrow_mut();
+        {
+            let mut current = current_rc.borrow_mut();
 
-        //Exit the loop if the target is found
-        if current.is_end(){
-            ending = Some(current.clone());
-            break;
+            //Exit the loop if the target is found
+            if current.is_end(){
+                ending = Some(current.clone());
+                break;
+            }
+
+            //Mark the current node as seen
+            current.seen = true;
         }
 
-        //Mark the current node as seen
-        current.seen = true;
-
-        //First check if the tentative distance of the current node is less than infinity
-        let curr_dist = current.distance;
+        let current = current_rc.borrow();
 
         //Iterate through the neighbours and calculate their tentative distance
-        for neighbour in current.edges.iter().filter(|n| !n.borrow().seen){
-            let mut neighbour_mut = neighbour.borrow_mut();
+        for neighbour_rc in current.edges.iter().filter(|n| !n.borrow().seen){
+            let mut neighbour = neighbour_rc.borrow_mut();
             
             //Calculate the new tentative distance (1 "unit" is the distance between 2 pixels)
-            let new_distance = curr_dist + 1;
+            let new_distance = current.distance + 1;
             
             //Update neighbour's tentative distance
-            if new_distance < neighbour_mut.distance{
-                neighbour_mut.distance = new_distance;
+            if new_distance < neighbour.distance{
+                neighbour.distance = new_distance;
             }
 
             //Update the neighbour's parent node 
-            neighbour_mut.previous = Some(Rc::new(RefCell::new(current.clone())));
+            neighbour.previous = Some(current_rc.clone());
 
             //Add the neighbour to the set of edges to expand
-            path_edges.push(neighbour.clone());
+            path_edges.push(neighbour_rc.clone());
         }
+
+        //Remove all the nodes that were already checked - they won't be used again
+        path_edges.retain(|n| !n.borrow().seen);
     }
 
     prepare_path(ending)
@@ -94,45 +94,47 @@ fn a_star(root: &Rc<RefCell<Node>>, n_nodes: usize) -> Result<Path>{
 
     //Loop while there are nodes (and subsequent paths) to expand
     while !path_edges.is_empty(){
-        //Remove all the nodes that were already checked - they won't be used again
-        path_edges.retain(|n| !n.borrow().seen);
 
         //Sort the vector and get the node with the smallest tentative distance
         path_edges.sort();
-        let current = path_edges[0].clone();
+        let current_rc = path_edges[0].clone();
 
-        let mut current = current.borrow_mut();
+        {
+            let mut current = current_rc.borrow_mut();
 
-        //Exit the loop if the target is found
-        if current.is_end(){
-            ending = Some(current.clone());
-            break;
-        }
+            //Exit the loop if the target is found
+            if current.is_end(){
+                ending = Some(current.clone());
+                break;
+            }
 
-        //Mark the current node as seen
-        current.seen = true;
+            //Mark the current node as seen
+            current.seen = true;
+        }    
 
-        //First check if the tentative distance of the current node is less than infinity
-        let curr_dist = current.distance;
+        let current = current_rc.borrow();
 
         //Iterate through the neighbours and calculate their tentative distance
-        for neighbour in current.edges.iter().filter(|n| !n.borrow().seen){
-            let mut neighbour_mut = neighbour.borrow_mut();
+        for neighbour_rc in current.edges.iter().filter(|n| !n.borrow().seen){
+            let mut neighbour = neighbour_rc.borrow_mut();
             
             //Calculate the new tentative distance + the heuristic distance
-            let new_distance = curr_dist + 1 + current.heuristic;
+            let new_distance = current.distance + 1 + neighbour.heuristic;
             
             //Update neighbour's tentative distance
-            if new_distance < neighbour_mut.distance{
-                neighbour_mut.distance = new_distance;
+            if new_distance < neighbour.distance{
+                neighbour.distance = new_distance;
             }
 
             //Update the neighbour's parent node 
-            neighbour_mut.previous = Some(Rc::new(RefCell::new(current.clone())));
+            neighbour.previous = Some(current_rc.clone());
 
             //Add the neighbour to the set of edges to expand
-            path_edges.push(neighbour.clone());
+            path_edges.push(neighbour_rc.clone());
         }
+
+        //Remove all the nodes that were already checked - they won't be used again
+        path_edges.retain(|n| !n.borrow().seen);
     }
 
     prepare_path(ending)
